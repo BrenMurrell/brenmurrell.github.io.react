99dd85440231ea90cf421ef969877d05b1dd2597
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-06-03 15:06:11 +1200

    adding react

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import {HashRouter as Router, Route} from 'react-router-dom'
 
 import Home from './Home'
+import Intro from './Intro'
 
 
 const App = () => (
   <Router>
     <div>
       <Route exact path='/' component={Home} />
+      <Route exact path='/' component={Intro} />
     </div>
   </Router>
 )
